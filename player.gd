14cda2c941dcf46dcad4ac98246bd0824d4ab170
Shuflduf <shuflduf@shuflduf.xyz>
2025-05-31 15:47:44 -0600

    bette rmovement

--- player.gd
+++ player.gd
@@ -2,27 +2,32 @@ extends CharacterBody2D
 
 @export var speed = 300
 
+var controller_index = 0
+
 func _physics_process(delta: float) -> void:
-	var input_dir = Input.get_joy_axis(0, JOY_AXIS_LEFT_X)
+	var input_dir = 0.0
+	if Input.get_connected_joypads().is_empty():
+		input_dir = Input.get_axis("left", "right")
+	else:
+		input_dir = Input.get_joy_axis(controller_index, JOY_AXIS_LEFT_X)
 	print(input_dir)
 	
-	velocity.x += input_dir
+	var jump_scale = (0.37/(scale.x+0.07)+0.65)
+	velocity.x = input_dir * speed * jump_scale
 	velocity.y += get_gravity().y * delta
 	
 	if Input.is_action_just_pressed("jump"):
-		velocity.y = -500
+		
+		velocity.y = -500 * jump_scale
 		
 	if Input.is_action_just_pressed("mitose"):
-		var new_player = self.duplicate()
-		new_player.velocity.y = -300
-		new_player.scale /= 2
-		var left_p = new_player.duplicate()
-		left_p.velocity.x = -100
-		var right_p = new_player.duplicate()
-		right_p.velocity.x = 100
-		new_player.queue_free()
-		get_parent().add_child(left_p)
-		get_parent().add_child(right_p)
+		for i in 2:
+			var p = self.duplicate()
+			p.velocity.y = -300
+			p.scale /= 2
+			p.velocity.x = 100 * (i * 2 - 1)
+		
+			get_parent().add_child(p)
 		queue_free()
 	
 	move_and_slide()
